ba7981a1dc259196add9a742497aaf5361edae5d
syahmifouzi <syahmi.fouzi@gmail.com>
2018-06-08 23:25:57 +0800

    me too

--- TUTORIAL1/main.go
+++ TUTORIAL1/main.go
@@ -29,6 +29,7 @@ func main() {
 	} else if vartext0 == "subtraction" {
 		operationsubtraction := int1 - int2
 		// fmt.Println("your answer is", operationsubtraction) hi!
+		// Im joining yeay!
 		printanswer(operationsubtraction)
 	} else if vartext0 == "multiplication" {
 		operationmultiply := int1 * int2
